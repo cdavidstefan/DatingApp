f1285a97ea83abe75812aa1220c95bb6df93826f
David <ciucadavidstefan@gmail.com>
2023-11-05 10:42:32 +0200

    Token Implementation

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,13 +1,25 @@
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegistered)
+            }
         }
     }
 }
