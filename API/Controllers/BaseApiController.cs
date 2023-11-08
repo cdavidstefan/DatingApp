f9ad9045b9c5b29959d37ca3720918ff32ce5a84
David <ciucadavidstefan@gmail.com>
2023-11-08 19:38:35 +0200

    Comment Added. Inheritance. DRY

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 
+
+// de ce am facut asta? every controller needs the ApiController and every controller needs a route, and every controller needs to derive from the controllerBase. inheritance in C#. DRY
+
 namespace API.Controllers
 {
     [ApiController]
