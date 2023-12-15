5cf5adb620d63808d5dc2b927663b908bd40e565
David <ciucadavidstefan@gmail.com>
2023-12-15 18:49:24 +0200

    Sorting,Filtering

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
